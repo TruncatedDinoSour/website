5073d9a99e919c2a4a432b1f9acb33ec9432b25c
Ari Archer <truncateddinosour@gmail.com>
2021-11-01 19:46:00 +0200

    update @ Mon  1 Nov 19:46:00 EET 2021

--- content/js/novpn/index.js
+++ content/js/novpn/index.js
@@ -4,5 +4,10 @@ const scroll_elements = document.getElementsByClassName("content");
 function scrollDown() {
     ++scroll_element;
     let elem = scroll_elements[scroll_element];
+    elem.scrollIntoView({ behavior: 'smooth' });
     if (elem) elem.scrollIntoView(true);
+}
+
+function scrollUp() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 }
