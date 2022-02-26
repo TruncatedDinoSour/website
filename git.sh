4d9cbec692dbba950d62a729445987d20c206a6d
Ari Archer <ari.web.xyz@gmail.com>
2022-02-26 15:09:26 +0200

    update @ Sat 26 Feb 15:09:26 EET 2022
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- git.sh
+++ git.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
 
-rm -fv content/styles/config/_main.css
+set -e
 
-git add .
-git commit -sam "update @ $(date)"
-git push -u origin "$(git rev-parse --abbrev-ref HEAD)"
+main() {
+    rm -fv content/styles/config/_main.css
+
+    git add .
+    git commit -sam "update @ $(date)"
+    git push -u origin "$(git rev-parse --abbrev-ref HEAD)"
+}
+
+main "$@"
