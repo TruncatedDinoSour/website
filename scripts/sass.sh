45b260429c58151401e08f351583c20f8e12fdb9
Ari Archer <ari.web.xyz@gmail.com>
2022-05-04 00:12:37 +0300

    update @ Wed  4 May 00:12:37 EEST 2022
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- scripts/sass.sh
+++ scripts/sass.sh
@@ -3,7 +3,9 @@
 set -e
 
 main() {
-    for file in $(find . -name '*.scss' -type f); do
+    for file in $(find . -name node_modules -prune -o -name '*.scss' -type f); do
+        [ -d "$file" ] && continue
+
         bnam="$(basename "$file")"
         out="$(dirname "$file")/${bnam%.*}.css"
 
