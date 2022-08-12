df2dcc80c7a8b2fe9813ac763603aa140e26df42
Ari Archer <ari.web.xyz@gmail.com>
2022-08-12 20:35:13 +0300

    Improve apis.json, add externap apis
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- scripts/apis.sh
+++ scripts/apis.sh
@@ -10,12 +10,16 @@ mkdata() {
 
 main() {
     printf ' * %s... ' 'Generating api list'
+    apid='api'
+    apis="$apid/apis.json"
+    eapis="$apid/external_apis.json"
 
+    # shellcheck disable=SC2094
     {
         printf '%s' '{"desc":"Ari-web API list","data":['
         find api -type f -exec basename {} \; | mkdata
-        echo '"."]}'
-    } >api/apis.json
+        echo "\"$(basename "$apis")\"],\"external\":$(cat -- "$eapis" | tr -d ' \n')}"
+    } >"$apis"
 
     echo 'done'
 }
