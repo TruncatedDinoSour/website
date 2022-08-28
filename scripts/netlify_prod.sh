990c02e8698d8225cadb4e343a634d5add2c9226
Ari Archer <ari.web.xyz@gmail.com>
2022-08-28 23:59:43 +0300

    Remove such verbose logging
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- scripts/netlify_prod.sh
+++ scripts/netlify_prod.sh
@@ -3,7 +3,7 @@
 set -e
 
 main() {
-    minify-all -s . -d min --skipFileExtensions=.png --logLevel=warn
+    minify-all -s . -d min --skipFileExtensions=.png --logLevel=fatal -p 10
     cp -r min/* .
     rm -r min
 }
